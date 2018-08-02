83ae482fcabb0b5be70cf85725f3d8f070c71a5e
ASUS <ASUS@DESKTOP-IHMILM6>
2018-08-02 11:57:37 +0700

    Initial Commit

---
+++ src/OperatorAritmatika2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class OperatorAritmatika2 {
+   public static void main(String[] args) {
+       double x = 10.0;
+       double y = 3.0;
+       double z = 2.5;
+       
+       double a = 0.0;
+       double b = 0.0;
+       
+       //OperasiMatematika
+       a = x / y;
+       b = x / z;
+       System.out.println("a=" + a);
+       System.out.println("b=" + b);
+    
+   }
+}
